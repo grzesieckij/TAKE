c5e0453398a13f062554154336cbf1e41f8eb991
grzesieckij <163602248+grzesieckij@users.noreply.github.com>
2024-06-07 13:09:24 +0200

    Added tasks from 8th and 9th labs.

---
+++ 8. Web Services (REST): JAX-RS (10.05)/Complaints/Server/src/main/java/lab/services/ModelMapperProducer.java
@@ -0,0 +1,16 @@
+package lab.services;
+
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.Dependent;
+import jakarta.enterprise.inject.Produces;
+import org.modelmapper.ModelMapper;
+
+@Dependent
+public class ModelMapperProducer {
+    @ApplicationScoped
+    @Produces
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
+}
